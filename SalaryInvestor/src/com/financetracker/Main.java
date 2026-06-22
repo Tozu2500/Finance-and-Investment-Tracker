3e0af57b0c50da34a4cdc2d29a023fac16f6e8f2
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-06-22 21:44:59 +0300

    start

--- SalaryInvestor/src/com/financetracker/Main.java
+++ SalaryInvestor/src/com/financetracker/Main.java
@@ -1,5 +1,22 @@
 package com.financetracker;
 
+import java.awt.Color;
+
 public class Main {
 
+    public static void main(String[] args) {
+        System.setProperty("swing.aatext", "true");
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("sun.java2d.opengl", "false");
+
+        try {
+            UIManager.put("ToolTip.background", Color.DARK_GRAY);
+
+        } catch (Exception ignored) {}
+
+        Thread.setDefaultUncaughtException((t, e) -> {
+            System.err.println("Uncaught exception on " + t.getName() + ":");
+            e.printStackTrace();
+        });
+    }
 }
