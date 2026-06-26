28761e2bcec113322d0110feb67d5b647e0f6848
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-06-26 22:51:37 +0300

    More refactoring... classes removed for now

--- backend/src/main/java/com/financetracker/model/RecurType.java
+++
@@ -1,7 +0,0 @@
-package com.financetracker.model;
-
-public enum RecurType {
-
-    NONE, DAILY, WEEKLY, MONTHLY, YEARLY;
-
-}
--- backend/src/main/java/com/financetracker/model/Role.java
+++
@@ -1,7 +0,0 @@
-package com.financetracker.model;
-
-public enum Role {
-
-    USER, ADMIN;
-
-}
