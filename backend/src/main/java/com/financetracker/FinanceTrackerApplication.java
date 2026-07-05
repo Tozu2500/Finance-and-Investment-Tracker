7dc790ca33a61f0a6149e1f965422a186cb70618
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-07-06 01:18:12 +0300

    Remove implements CommandLineRunner

--- backend/src/main/java/com/financetracker/FinanceTrackerApplication.java
+++ backend/src/main/java/com/financetracker/FinanceTrackerApplication.java
@@ -1,11 +1,10 @@
 package com.financetracker;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinanceTrackerApplication implements CommandLineRunner {
+public class FinanceTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FinanceTrackerApplication.class, args);
